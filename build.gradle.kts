fc722840b41431021c39487f2c157f599723bfdb
Pavel Sergeev <pavel.sergeev@jetbrains.com>
2020-12-02 19:38:50 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    java
+}
+group = "me.pavel"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+task("day1_1", JavaExec::class) {
+    main = "Day1Task1Kt"
+    classpath = sourceSets["main"].runtimeClasspath
+}
+
+task("day1_2", JavaExec::class) {
+    main = "Day1Task2Kt"
+    classpath = sourceSets["main"].runtimeClasspath
+}
+
+task("day2_1", JavaExec::class) {
+    main = "Day2Task1Kt"
+    classpath = sourceSets["main"].runtimeClasspath
+}
+
+task("day2_2", JavaExec::class) {
+    main = "Day2Task2Kt"
+    classpath = sourceSets["main"].runtimeClasspath
+}
